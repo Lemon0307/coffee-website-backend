c59eb6d2dbd7803ee09de93ff44f8fc758f29321
Lemon2027 <58172653+Lemon0307@users.noreply.github.com>
2023-01-31 19:26:09 +0000

    Pending changes exported from your codespace

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const User = require('../schemas/User');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
 router.post('/sign-up', bodyParser.json(), async(req, res) => {
     const {firstName, surname, username, email, password} = req.body
@@ -10,12 +11,18 @@ router.post('/sign-up', bodyParser.json(), async(req, res) => {
         if (docs.length) {
             res.status(403).json({message: 'This email already exists'})
         } else {
-            const user = new User({firstName, surname, username, email, password})
-            await user.save()
+            bcrypt.hash(password, 10, async(err, hash) => {
+                const user = new User({firstName, surname, username, email, hash})
+                await user.save()
+            })
             res.json({url: "/login", message: "Successfuly signed up!"})
             console.log('a new user has been created')
         }
     })
+})
+
+router.post('/login', bodyParser.json(), async(req, res) => {
+    
 })
 
 module.exports = router
